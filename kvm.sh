0c6279a24bfd7f5774687965575f827e79154406
chickentom <49533314+chickentom@users.noreply.github.com>
2021-06-15 12:10:33 +0200

    Add files via upload

---
+++ kvm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+temp=$(LC_ALL=C lscpu |grep Virtualization)
+echo $temp
+
+if [[ $temp == *VT-x* || $temp == *AMD* ]];
+then 
+    echo "Your PC is not compatable or you haven't enabled Virtualization Support"
+    exit
+fi
+OS=$(lsb_release -a)
+if [$OS == *"lsb_release -a"*]
+then
+    echo "Ubuntu"
+exit
