1011ff6445287ad260a5bf6033ac5887957a446b
chickentom <49533314+chickentom@users.noreply.github.com>
2021-09-13 00:47:19 +0200

    Update kvm.sh

--- kvm.sh
+++ kvm.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 temp=$(LC_ALL=C lscpu |grep Virtualization)
 OS=$(lsb_release -a)
-echo $temp
-echo $OS
+
 if [[ $temp == *VT-x* || $temp == *AMD* ]];
 then 
     echo "Your PC is compatable"
@@ -14,6 +13,16 @@ then
     if [[ $OS == *Manjaro* ]];
     then
     echo "You're runnin Manjaro"
+    sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
+    sudo pacman -S ebtables iptables
+    echo -e "[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf
+    sudo pacman -Syy
+    sudo pacman -S yaourt
+    yaourt -S --noconfirm --needed libguestfs
+    sudo systemctl enable libvirtd.service
+    sudo systemctl start libvirtd.service
+    echo "---------------------------------------------"
+    
     exit
     fi
 else
